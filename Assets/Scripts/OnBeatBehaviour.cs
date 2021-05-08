624cb55ba2d1efd269dd90e3016fa23e2d82f466
Luca Martinelli <lucxmangajet@gmail.com>
2021-05-08 13:56:41 +0200

    Added base architecture for sound system

--- Assets/Scripts/OnBeatBehaviour.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class OnBeatBehaviour : MonoBehaviour
-{
-    protected abstract void OnBeat();
-
-}
